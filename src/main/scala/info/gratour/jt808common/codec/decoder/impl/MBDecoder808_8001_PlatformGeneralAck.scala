16fb462405da220fa24b1498eb7b790e24c7be06
KwanKin Yau <alphax@vip.163.com>
2021-09-01 17:30:24 +0800

    1) add 8001 decode support

---
+++ src/main/scala/info/gratour/jt808common/codec/decoder/impl/MBDecoder808_8001_PlatformGeneralAck.scala
@@ -0,0 +1,22 @@
+/** *****************************************************************************
+ * Copyright (c) 2019, 2021 lucendar.com.
+ * All rights reserved.
+ *
+ * Contributors:
+ * KwanKin Yau (alphax@vip.163.com) - initial API and implementation
+ * ***************************************************************************** */
+package info.gratour.jt808common.codec.decoder.impl
+
+import info.gratour.jt808common.codec.decoder.JT808MsgBodyDecoder
+import info.gratour.jt808common.protocol.msg.JT808Msg_8001_PlatformGeneralAck
+import io.netty.buffer.ByteBuf
+
+object MBDecoder808_8001_PlatformGeneralAck extends JT808MsgBodyDecoder[JT808Msg_8001_PlatformGeneralAck]{
+
+  override def decodeMsgBody(m: JT808Msg_8001_PlatformGeneralAck, body: ByteBuf, tempBuf: Array[Byte]): Unit = {
+    m.setAckSeqNo(body.readUnsignedShort())
+    m.setAckMsgId(body.readUnsignedShort())
+    m.setResult(body.readByte())
+  }
+
+}
